d27a52bdd0317850779cd1bdc9a336447b87817e
paJavier <pajavier@mcm.edu.ph>
2026-09-22 12:47:11 +0800

    connected supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js"
 import { projectId, publicAnonKey } from "../../utils/supabase/info"
 
+const supabaeURL = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+
 export const supabase = createClient(
   `https://${projectId}.supabase.co`,
   publicAnonKey
